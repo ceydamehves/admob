613360bb335e6991b2b80e4543b51a07d033c00e
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2019-08-21 15:12:37 +0300

    project created.

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+
+class HomeScreen extends React.Component{
+   
+    render (){
+        return(
+
+            <Text>Admob deneme</Text>
+              
+        );
+    }
+}
+export default HomeScreen;
