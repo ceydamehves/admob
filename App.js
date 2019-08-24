7707f6cafc3267461cb27a4773051714cac53a06
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2019-08-24 15:55:47 +0300

    admob error fixed

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import React from "react";
 import HomeScreen from "C:/Admob/src/screens/HomeScreen.js";
 import { View } from "react-native";
 
+
 export default class App extends React.Component {
   render() {
     return (
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import {View, Text, ImageBackground, StyleSheet} from 'react-native';
-
+import { 
+    AdMobBanner, 
+    AdMobInterstitial, 
+    PublisherBanner,
+    AdMobRewarded
+  } from 'react-native-admob'
+  
 class HomeScreen extends React.Component{
    
     render (){
         return(
+            <View>
 
             <Text>Admob deneme</Text>
-              
+            <AdMobBanner
+   adSize="smartBannerPortrait"
+  adUnitID="ca-app-pub-3940256099942544/6300978111"
+  testDevices={["EMULATOR"]}
+  onAdFailedToLoad={error => console.error(error)}
+/>
+              </View>
         );
     }
 }
